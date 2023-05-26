e37c56261b1c251024de4a6aec9fff89e6e57c6e
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-26 09:50:47 +0530

    without lambda

---
+++ src/CalculatorImpl.java
@@ -0,0 +1,6 @@
+public class CalculatorImpl implements Calculator{
+    @Override
+    public void switchOn() {
+        System.out.println("switch is now on");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CalculatorImpl calculator = new CalculatorImpl();
+        calculator.switchOn();
+    }
+}
